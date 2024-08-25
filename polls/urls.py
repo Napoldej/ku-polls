d02195652209316461ee3d0caf8c9648e3de6ca6
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-08-25 13:57:21 +0700

    implement tutorial 4

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path('',views.index,name = "index"),
-    path("<int:question_id>/",views.details, name = "detail"),
-    path("<int:question_id>/result/",views.results, name = "result"),
-    path("<int:question_id>/vote/", views.vote, name = "vote")
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='details'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     
 ]
