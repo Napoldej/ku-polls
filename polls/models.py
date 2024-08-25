5de1004555322b207ef0c5c3b7d00ab0cd35c0ff
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-08-25 12:17:46 +0700

    implement tutorial 3

--- polls/models.py
+++ polls/models.py
@@ -7,21 +7,23 @@ from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
-    question = models.CharField(max_length=250)
+    question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     def __str__(self):
-        return (f"{str(self.id)}, Question:{self.question}, Pub_date:{self.pub_date}")
+        return (f"{str(self.id)}, Question:{self.question_text}, Pub_date:{self.pub_date}")
     
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_text = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=250)
     vote = models.IntegerField(default=0)
     
     def __str__(self):
         return (f"{self.choice_text}")
+    
+    
     
     
